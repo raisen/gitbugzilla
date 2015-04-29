a1280647b6db087077ce51b11c880ccc5aeabe2f
Weydson Lima <weydson@raisenllc.com>
2015-04-29 02:54:23 -0700

    use environment variables

--- README.md
+++ README.md
@@ -6,21 +6,22 @@ Requires:
 
 - [python-bugzilla](https://fedorahosted.org/python-bugzilla/)
 
-## Usage
-### Generate a text file using python-bugzilla
+## Installation
 
-Generate a text file containing a list of bugs from Bugzilla using `python-bugzilla`. For example:
+Set the following environment variables to match with your Bugzilla's setup:
 
-    bugzilla --bugzilla=http://MY_BUGZILLA_URL/xmlrpc.cgi --user='MY_BUGZILLA_USERNAME' --password='MY_BUGZILLA_PASSWORD' query > bugs.txt
+**BUGZILLA_URL** = Your Bugzilla's URL. URL should end with xmlrpc.cgi, such as http://bugzilla/xmlrpc.cgi
+**BUGZILLA_USERNAME** = Your Bugzilla's username
+**BUGZILLA_PASSWORD** = Your Bugzilla's password
 
-Command above will create a text file ``bugs.txt`` with a list of all the bugs from Bugzilla.
-
-### Process text file
-Invoke the shell script:
-    ./gitbugzilla.sh
-
-## Settings
 Shell script has some variables you might want to customize:
 
 - **FILE** - Text file created by python-bugzilla (defaults to *bugs.txt*)
 - **bug_prefix** - Prefix when matching Bugzilla's bug number with git's branch name (defaults to *bug*). If value is kept as default and say that we want to update the branch corresponding to Bugzilla's bug number 100, a branch named *bug100* must exist, otherwise branch won't have description set.
+
+## Usage
+
+    gitbugzilla.sh <COMMAND>
+
+      -s Show git branches with their descriptions
+      -g Populate git branches with Bugzilla's information"
--- gitbugzilla.sh
+++ gitbugzilla.sh
@@ -1,29 +1,86 @@
 #!/bin/sh
-FILE=bugs.txt
-branches=($(git branch))
-bug_prefix="bug"
 
-#echo $branches
+# Settings
+FILE=/tmp/bugs.txt
+BRANCHES=($(git branch))
+BUGPREFIX="bug"
+BUGZILLA="bugzilla"
 
-while read -a fields
-do
- bug_no=${fields[0]#?}
- status=${fields[1]}
- author=${fields[3]}
- for i in {0..4} 
- do
-  unset fields[$i]
- done
- description=${fields[@]}
- description="${description//\"/\\\"}"
- branch_name="$bug_prefix$bug_no"
- FOUND_BRANCH=( ${branches[@]/$branch_name} )
- if ! [ ${#FOUND_BRANCH[@]} != ${#branches[@]} ]; then
-#  echo "The branch '"$branch_name"' could not be found"
- else
-#  echo "The branch '"$branch_name"' could be found!"
-  x="git config branch.${branch_name}.description \"$status $description\""
-#  echo $x
-  eval $x
- fi
-done < $FILE
+VERSION="1.1"
+
+function change_descriptions() {
+  while read -a fields; do
+    local bug_no=${fields[0]#?}
+    local status=${fields[1]}
+    local author=${fields[3]}
+    for i in {0..4}; do
+      unset fields[$i]
+    done
+
+    local description=${fields[@]}
+    description="${description//\"/\\\"}"
+    local branch_name="$BUGPREFIX$bug_no"
+
+    local found_branch=( ${BRANCHES[@]/$branch_name} )
+
+    if ! [ ${#found_branch[@]} == ${#BRANCHES[@]} ]; then
+      x="git config --replace-all branch.${branch_name}.description \"$status $description\""
+      eval $x
+    fi
+  done < $FILE
+}
+
+# Shows branches with descriptions
+function show_descriptions() {
+  branches=$(git for-each-ref --format='%(refname)' refs/heads/ | sed 's|refs/heads/||')
+  for branch in $branches; do
+    desc=$(git config branch.$branch.description)
+    if [ $branch == $(git rev-parse --abbrev-ref HEAD) ]; then
+      branch="* \033[0;32m$branch\033[0m"
+     else
+       branch="  $branch"
+     fi
+     echo "$branch \033[0;36m$desc\033[0m"
+  done
+}
+
+function generate_text_file() {
+  $BUGZILLA --bugzilla=$BUGZILLA_URL --user="$BUGZILLA_USERNAME" --password="$BUGZILLA_PASSWORD" query > $FILE
+  change_descriptions
+}
+
+function remove_temp_file() {
+  rm $FILE  
+}
+
+function show_help() {
+  echo "gitbugzilla, version $VERSION
+
+usage: gitbugzilla <COMMAND>
+
+  -s Show git branches with their descriptions
+  -g Populate git branches with Bugzilla's information"
+}
+
+options_found=0
+
+while getopts sg opt; do
+  options_found=1
+  case $opt in
+  s)
+    show_descriptions
+    ;;
+  g)
+    generate_text_file
+    change_descriptions
+    remove_temp_file
+    ;;
+  *)
+    echo 'a'
+    ;;
+  esac
+done
+
+if ((!options_found)); then
+  show_help
+fi
